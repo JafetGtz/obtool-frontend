bfdf4ce33feb0cb1dcb1cdce933bda3eb43bf40e
JafetGtz <gtzjafet@gmail.com>
2023-07-25 18:13:40 -0600

    get unidades

--- js/cliente.js
+++ js/cliente.js
@@ -22,3 +22,63 @@ function guardar() {
     return;
   }
 }
+
+
+const headers = {'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*',
+                    'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
+
+const getUnits = async  () => {
+    const endpoint = 'https://obtool.bustrax.io/unidades_negocio';
+    try {
+      const response = await fetch(endpoint, {
+        headers: headers
+      });
+  
+      // Verificar el estado de la respuesta
+      if (!response.ok) {
+        throw new Error('La solicitud no fue exitosa');
+      }
+  
+      // Parsear la respuesta a JSON
+      const data = await response.json();
+      console.log(data, 'data')
+      return data;
+    } catch (error) {
+      console.error('Error al obtener los datos:', error);
+      throw error;
+    }
+  }
+
+
+  getUnits()
+
+
+
+
+  async function populateSelectWithUnidades() {
+    try {
+      // Obtener los datos usando el método fetchData que definimos antes
+      const data = await getUnits();
+  
+      // Obtener la referencia al elemento select
+      const selectElement = document.getElementById('tipoCliente');
+  
+      // Iterar sobre los objetos del array "unidades" y crear las opciones
+      data.contenido.forEach((unidad) => {
+        const option = document.createElement('option');
+        option.value = unidad.code; // Usamos el campo "code" como valor de la opción
+        option.textContent = unidad.description; // Usamos el campo "description" como texto de la opción
+        selectElement.appendChild(option);
+      });
+    } catch (error) {
+      console.error('Error al llenar el select:', error);
+    }
+  }
+
+
+
+const sendData =  async ( {  }) => {
+            console.log('Enviar datos formulario')
+}
+
